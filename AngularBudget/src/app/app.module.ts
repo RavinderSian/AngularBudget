4279a7b4d0dbf4db5ead995123f9f963c5cfc82f
ravinder sian <rsian761@gmail.com>
2023-11-17 19:41:25 +0000

    add initial components

--- AngularBudget/src/app/app.module.ts
+++ AngularBudget/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderbarComponent } from './headerbar/headerbar.component';
+import { BudgetlistComponent } from './budgetlist/budgetlist.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderbarComponent,
+    BudgetlistComponent,
+    BudgetListComponent,
+    HeaderBarComponent
   ],
   imports: [
     BrowserModule
