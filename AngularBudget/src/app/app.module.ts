a56ba6024910a3e48489d2dec88aaa05425f929b
ravinder sian <rsian761@gmail.com>
2023-11-22 15:45:39 +0000

    add header bar

--- AngularBudget/src/app/app.module.ts
+++ AngularBudget/src/app/app.module.ts
@@ -2,23 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderbarComponent } from './headerbar/headerbar.component';
-import { BudgetlistComponent } from './budgetlist/budgetlist.component';
 import { BudgetListComponent } from './budget-list/budget-list.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderbarComponent,
-    BudgetlistComponent,
+    HeaderBarComponent,
     BudgetListComponent,
-    HeaderBarComponent
+    BudgetListComponent,
+    HeaderBarComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
