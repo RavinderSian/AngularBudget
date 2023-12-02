6c70c725d174e0ac0d96ea6809a76a8631256560
ravinder sian <rsian761@gmail.com>
2023-12-02 11:45:20 +0000

    set up containers properly

--- AngularBudget/src/app/month-filter/month-filter.component.ts
+++ AngularBudget/src/app/month-filter/month-filter.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-month-filter',
   templateUrl: './month-filter.component.html',
-  styleUrls: ['./month-filter.component.css']
+  styleUrls: ['./month-filter.component.css'],
 })
-export class MonthFilterComponent {
+export class MonthFilterComponent implements OnInit {
+  currentYear!: number;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+  }
 }
