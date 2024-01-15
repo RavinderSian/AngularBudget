315696a4ec97b92f0785cdf78a23e3f6cad5d149
ravinder sian <rsian761@gmail.com>
2024-01-15 20:49:38 +0000

    update expense service names and add to expense lise

--- AngularBudget/src/app/components/expense-list/expense-list.component.ts
+++ AngularBudget/src/app/components/expense-list/expense-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ExpenseService } from 'src/app/services/expense/expense.service';
 
 @Component({
   selector: 'app-expense-list',
   templateUrl: './expense-list.component.html',
-  styleUrls: ['./expense-list.component.css']
+  styleUrls: ['./expense-list.component.css'],
 })
 export class ExpenseListComponent {
+  public service: ExpenseService;
 
+  constructor(service: ExpenseService) {
+    this.service = service;
+  }
 }
