de31fbd827fcbf004c8751f09a164f39faa2ccdd
ravinder sian <rsian761@gmail.com>
2024-01-15 19:54:14 +0000

    add expense model to component

--- AngularBudget/src/app/components/expense/expense.component.ts
+++ AngularBudget/src/app/components/expense/expense.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Expense } from 'src/app/models/expense';
 
 @Component({
   selector: 'app-expense',
   templateUrl: './expense.component.html',
-  styleUrls: ['./expense.component.css']
+  styleUrls: ['./expense.component.css'],
 })
 export class ExpenseComponent {
+  public expense: Expense;
 
+  constructor(expense: Expense) {
+    this.expense = expense;
+  }
 }
