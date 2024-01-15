55f243d75a0af0cb4e76a3e84a245d7c84b2813c
ravinder sian <rsian761@gmail.com>
2024-01-15 19:28:03 +0000

    move components into components folder

--- AngularBudget/src/app/app.module.ts
+++ AngularBudget/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AddExpenseContainerComponent } from './add-expense-container/add-expense-container.component';
+import { AddExpenseContainerComponent } from './components/add-expense-container/add-expense-container.component';
 import { AppComponent } from './app.component';
-import { BudgetContainerComponent } from './budget-container/budget-container.component';
-import { DateFilterComponent } from './date-filter-container/date-filter-container.component';
-import { ExpenseListComponent } from './expense-list/expense-list.component';
-import { HeaderBarComponent } from './header-bar/header-bar.component';
-import { MonthFilterContainerComponent } from './month-filter-container/month-filter-container.component';
-import { MonthFilterComponent } from './month-filter/month-filter.component';
-import { YearFilterContainerComponent } from './year-filter-container/year-filter-container.component';
-import { YearFilterComponent } from './year-filter/year-filter.component';
-import { ExpenseComponent } from './expense/expense.component';
+import { BudgetContainerComponent } from './components/budget-container/budget-container.component';
+import { DateFilterComponent } from './components/date-filter-container/date-filter-container.component';
+import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { MonthFilterContainerComponent } from './components/month-filter-container/month-filter-container.component';
+import { MonthFilterComponent } from './components/month-filter/month-filter.component';
+import { YearFilterContainerComponent } from './components/year-filter-container/year-filter-container.component';
+import { YearFilterComponent } from './components/year-filter/year-filter.component';
+import { ExpenseComponent } from './components/expense/expense.component';
 
 @NgModule({
   declarations: [
