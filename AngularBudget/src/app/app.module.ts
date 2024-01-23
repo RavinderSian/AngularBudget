0ff12834a0f4f2ce096611d088dd77ac6ea7cd06
ravinder sian <rsian761@gmail.com>
2024-01-23 21:59:07 +0000

    allow vars to pass but not looking good

--- AngularBudget/src/app/app.module.ts
+++ AngularBudget/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AddExpenseContainerComponent } from './components/add-expense-container/add-expense-container.component';
 import { AppComponent } from './app.component';
+import { AddExpenseContainerComponent } from './components/add-expense-container/add-expense-container.component';
 import { BudgetContainerComponent } from './components/budget-container/budget-container.component';
 import { DateFilterComponent } from './components/date-filter-container/date-filter-container.component';
+import { ExpenseListHeaderComponent } from './components/expense-list-header/expense-list-header.component';
 import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { ExpenseComponent } from './components/expense/expense.component';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 import { MonthFilterContainerComponent } from './components/month-filter-container/month-filter-container.component';
 import { MonthFilterComponent } from './components/month-filter/month-filter.component';
 import { YearFilterContainerComponent } from './components/year-filter-container/year-filter-container.component';
 import { YearFilterComponent } from './components/year-filter/year-filter.component';
-import { ExpenseComponent } from './components/expense/expense.component';
-import { ExpenseListHeaderComponent } from './components/expense-list-header/expense-list-header.component';
+import { ExpenseService } from './services/expense/expense.service';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,7 @@ import { ExpenseListHeaderComponent } from './components/expense-list-header/exp
     ExpenseListHeaderComponent,
   ],
   imports: [BrowserModule],
-  providers: [],
+  providers: [ExpenseService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
